f265a0876a1ff04fe702ba23f83078e50d49346c
yassine <yassine@localhost>
2017-03-04 00:21:46 +0100

    Mise en place du connecteur

---
+++ MultiRepoProject/src/main/java/fr/bpm/actia/gestionbesoin/connectors/GestionBesoinConnector.java
@@ -0,0 +1,10 @@
+package fr.bpm.actia.gestionbesoin.connectors;
+
+public class GestionBesoinConnector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
